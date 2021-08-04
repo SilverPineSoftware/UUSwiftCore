cca4b25249824629db40d79602521ad41fb8e65d
Jonathan Hays <jon@silverpinesoftware.com>
2021-08-03 18:15:30 -0700

    Fixing up the directory structure

---
+++ UUSwiftCore/UUSwiftCore.h
@@ -0,0 +1,18 @@
+//
+//  UUSwiftCore.h
+//  UUSwiftCore
+//
+//  Created by Jonathan Hays on 8/3/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UUSwiftCore.
+FOUNDATION_EXPORT double UUSwiftCoreVersionNumber;
+
+//! Project version string for UUSwiftCore.
+FOUNDATION_EXPORT const unsigned char UUSwiftCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UUSwiftCore/PublicHeader.h>
+
+
