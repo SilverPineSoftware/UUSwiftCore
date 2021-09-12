028ebae602272ac68c0ae8840b2269f14fd1a58b
Jonathan Hays <jon@silverpinesoftware.com>
2021-09-12 15:39:41 -0700

    Adding password functionality to UUKeychain

--- UUSwiftCore/UUKeychain.swift
+++ UUSwiftCore/UUKeychain.swift
@@ -72,6 +72,49 @@ public class UUKeychain: NSObject
         {
         }
     }
+
+	public class func setPassword(_ password : String, forService service: String, forAccount account: String) {
+
+		let dictionary = NSMutableDictionary()
+		dictionary.setObject(kSecClassGenericPassword, forKey: kSecClass as NSString)
+		dictionary.setObject(service, forKey: kSecAttrService as NSString)
+		dictionary.setObject(account, forKey: kSecAttrAccount as NSString)
+		dictionary.setObject(password, forKey: kSecValueData as NSString)
+
+		var result: CFTypeRef? = nil
+		let status = SecItemAdd(dictionary, &result)
+
+		if (status == errSecSuccess && result != nil) {
+			print("Successfully saved")
+		}
+		else {
+			print("Could not save password")
+		}
+	}
+
+	public class func password(forService service: String, forAccount account: String) -> String? {
+
+		let dictionary = NSMutableDictionary()
+		dictionary.setObject(kSecClassGenericPassword, forKey: kSecClass as NSString)
+		dictionary.setObject(service, forKey: kSecAttrService as NSString)
+		dictionary.setObject(account, forKey: kSecAttrAccount as NSString)
+		dictionary.setObject(true, forKey: kSecReturnData as NSString)
+		dictionary.setObject(kSecMatchLimitOne, forKey: kSecMatchLimit as NSString)
+
+		var result: CFTypeRef? = nil
+		let status = SecItemCopyMatching(dictionary, &result)
+
+		if (status == errSecSuccess && result != nil)
+		{
+			if let data = result as? Data {
+				let string = String(data: data, encoding: .utf8)
+				return string
+			}
+		}
+
+		return nil
+
+	}
     
     public class func tryGetString(key: String) throws -> String?
     {
