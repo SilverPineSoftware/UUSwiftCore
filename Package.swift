7bb47980e5784d62dfb62addd780b9c7bbf964aa
Ryan DeVore <ryan@silverpine.com>
2025-05-16 22:42:10 -0700

    Add type dynamic

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
 		// Products define the executables and libraries a package produces, and make them visible to other packages.
 		.library(
 			name: "UUSwiftCore",
+            type: .dynamic,
 			targets: ["UUSwiftCore"]),
 	],
     
