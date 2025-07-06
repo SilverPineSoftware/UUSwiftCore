919c48b49af5720fa58bf4a9e637e00b3b0ea9de
Ryan DeVore <ryan@silverpine.com>
2025-07-06 07:25:28 -1000

    Add UUErrorBlock

--- UUSwiftCore/UUTypeAliases.swift
+++ UUSwiftCore/UUTypeAliases.swift
@@ -7,5 +7,6 @@
 
 import Foundation
 
+public typealias UUErrorBlock<T> = ((Error?) -> Void)
 public typealias UUObjectErrorBlock<T> = ((T?, Error?) -> Void)
 public typealias UUListErrorBlock<T> = (([T]?, Error?) -> Void)
