568790bfb132a77a3b3c6a18f6f747222ef709a0
Ryan DeVore <ryan@silverpine.com>
2025-07-05 22:23:13 -1000

    Add some common type alias definitions

---
+++ UUSwiftCore/UUTypeAliases.swift
@@ -0,0 +1,11 @@
+//
+//  UUTypeAliases.swift
+//  UUSwiftCore
+//
+//  Created by Ryan DeVore on 7/5/25.
+//
+
+import Foundation
+
+public typealias UUObjectErrorBlock<T> = ((T?, Error?) -> Void)
+public typealias UUListErrorBlock<T> = (([T]?, Error?) -> Void)
