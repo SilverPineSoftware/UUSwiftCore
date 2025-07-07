0e99aed50da178c954b7982896619210d8ac8854
Ryan DeVore <ryan@silverpine.com>
2025-07-07 12:42:02 -1000

    Add object and list blocks

--- UUSwiftCore/UUTypeAliases.swift
+++ UUSwiftCore/UUTypeAliases.swift
@@ -9,5 +9,7 @@ import Foundation
 
 public typealias UUVoidBlock = (() -> Void)
 public typealias UUErrorBlock = ((Error?) -> Void)
+public typealias UUObjectBlock<T> = ((T) -> Void)
+public typealias UUListBlock<T> = (([T]) -> Void)
 public typealias UUObjectErrorBlock<T> = ((T?, Error?) -> Void)
 public typealias UUListErrorBlock<T> = (([T]?, Error?) -> Void)
